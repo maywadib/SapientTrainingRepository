10d5f6ef4dc831d4174db8eea2f7ea9b9eb6e5d2
maywadib <maywadib@WKWIN8467072.global.publicisgroupe.net>
2019-07-19 15:37:28 +0530

    Week 1 Sprint

--- src/SmartCoffeeMachine/Coffee.java
+++ src/SmartCoffeeMachine/Coffee.java
@@ -1,7 +1,65 @@
 package SmartCoffeeMachine;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+
 public abstract class Coffee {
+	public static HashMap<String , Integer> coffCounter = new HashMap<>();
+	static {
+	coffCounter.put("cappuccino",0);
+	coffCounter.put("espresso",0);
+	coffCounter.put("latte",0);
+	}
+	  
+		public static List<String> getPopularCoffeeOption() {
+				
+				
+
+				Set entrySet = Coffee.coffCounter.entrySet();
+				Iterator itr = entrySet.iterator();
+				
+				int hasMaxCount = 0;
+				String popularBeverage = null;
+				
+				while(itr.hasNext()) {
+					Entry<String, Integer> next = (Entry) itr.next();
+					if(next.getValue() >= hasMaxCount) {
+						hasMaxCount = next.getValue();
+						popularBeverage = next.getKey();
+					}
+				}
+				
+				List<String> popularList = new ArrayList<>();
+//				popularList.add(popularBeverage);
+				Iterator itr2 = entrySet.iterator();
+				
+				while(itr2.hasNext()) {
+					Entry<String, Integer> next = (Entry) itr2.next();
+					if(next.getValue() == hasMaxCount) {
+						popularList.add(next.getKey());
+					}
+				}
+				
+				return popularList;
+				
+				
+			} 
+		 
+
+	
+	
 	public int milkContain;
 	public int coffeeContain;
-	public void display() {}
+
+	public void display() {
+	}
+
+	public static void dispalyOptions() {
+		System.out.println(
+				"Welcome to the Smart Coffee Machine! \nPlease select a choice \n1. Cappuccino \n2. Espresso \n3. Latte \n4. Make your own coffee. \n5.Our recommendation for you.");
+	}
 }
--- src/SmartCoffeeMachine/FirstScreen.java
+++ src/SmartCoffeeMachine/FirstScreen.java
@@ -20,19 +20,19 @@ public class FirstScreen {
 
 		}
 
-		while(true) {
+		while (true) {
 
 			// Scanner sc = new Scanner(System.in);
 			BufferedReader sc = new BufferedReader(new InputStreamReader(System.in));
 
-			
 			System.out.println("Please Enter your LLID");
 			String llid = sc.readLine();
-			
+
 			if (llid.equals("exit")) {
 				System.exit(0);
 			}
-			
+
+			int flag = 0;
 			// check llid in the database
 			if (db.isUserPresent(llid)) {
 				System.out.println("Please Enter your password");
@@ -41,22 +41,22 @@ public class FirstScreen {
 				String password = passInp.readLine();
 
 				int count = 0;
-				while(count<2)
-				{
-					if(db.checkPassword(llid, password))
+				while (count < 2) {
+					if (db.checkPassword(llid, password)) {
+
+						flag =1;
 						break;
-					else {
+					} else {
 						System.out.println("Please enter again");
 						password = sc.readLine();
 					}
 					count++;
 				}
-				
-				if(count==3)
-				{
+
+				if (count == 2) {
 					System.out.println("Incorrect password for three times");
 				}
-				
+
 			} else {
 				System.out.println("User not found...Please Sign up");
 
@@ -64,9 +64,66 @@ public class FirstScreen {
 				String password = sc.readLine();
 
 				db.signUp(llid, password);
+				
+				flag = 1;
 
 			}
-		}
+			
+			
+			
+			
+			if(flag==1)
+			{
+				Coffee.dispalyOptions();
+				int option = Integer.parseInt(sc.readLine());
 
+				switch (option) {
+				case 1: {
+					Coffee cap = new Cappuccino();
+					Coffee.coffCounter.put("cappuccino",Coffee.coffCounter.get("cappuccino")+1);
+					cap.display();
+					break;
+				}
+				case 2: {
+					
+					Coffee esp = new Espresso();
+					Coffee.coffCounter.put("espresso",Coffee.coffCounter.get("espresso")+1);
+					esp.display();
+
+					break;
+				}
+				case 3: {
+					
+					Coffee lat = new Lattee();
+					Coffee.coffCounter.put("latte",Coffee.coffCounter.get("latte")+1);
+					lat.display();
+					break;
+				}
+				case 4: {
+					
+					System.out.println("Work in Progress , Sorry for the inconvience: ");
+//					Coffee yc = new YourChoice(
+//							);
+//					yc.display();
+					break;
+				}
+				case 5: {
+
+					System.out.println(Coffee.getPopularCoffeeOption());
+					System.out.println("Thank You for using the Smart Coffee Machine...\n");
+					break;
+				}
+				default:
+					System.out.println("Wrong Option");
+				}
+
+			}
+			
+			
+			
+			
+		System.out.println("Current coffee count for all the available option : " +Coffee.coffCounter);
+		}
+		
 	}
 }
